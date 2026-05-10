f690f7561e947db1c769f896ae15bb6b6c7ed7c1
smerteliko <smerteliko@gmail.com>
2026-05-10 16:57:06 +0300

    feat: Native desktop notifications for long-running tasks
    Integrated notify-send (Linux) and AppleScript (macOS) support. Added an automatic observer in the execution lifecycle that sends a desktop notification if a command takes longer than 5 seconds.

--- src/Helper/NotificationHelper.php
+++ src/Helper/NotificationHelper.php
@@ -12,7 +12,6 @@ class NotificationHelper
 		$os = PHP_OS_FAMILY;
 
 		if ($os === 'Linux') {
-			// Ubuntu/Debian использует notify-send
 			$command = sprintf(
 				'notify-send -i %s %s %s',
 				escapeshellarg($icon),
@@ -21,13 +20,26 @@ class NotificationHelper
 			);
 			shell_exec($command);
 		} elseif ($os === 'Darwin') {
-			// macOS использует AppleScript
 			$command = sprintf(
 				'osascript -e "display notification %s with title %s"',
 				escapeshellarg($message),
 				escapeshellarg($title)
 			);
 			shell_exec($command);
+		} elseif ($os === 'Windows') {
+			$psCommand = sprintf(
+				'[reflection.assembly]::loadwithpartialname("System.Windows.Forms"); ' .
+				'[reflection.assembly]::loadwithpartialname("System.Drawing"); ' .
+				'$n = new-object system.windows.forms.notifyicon; ' .
+				'$n.icon = [system.drawing.systemicons]::Information; ' .
+				'$n.visible = $true; ' .
+				'$n.showballoontip(5000, %s, %s, [system.windows.forms.tooltipicon]::Info);',
+				var_export($title, true),
+				var_export($message, true)
+			);
+
+			$command = sprintf('powershell -Command "& { %s }"', $psCommand);
+			exec($command);
 		}
 	}
 }
